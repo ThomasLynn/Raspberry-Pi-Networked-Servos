1658ecfc9a05f7787db48f7893e79f99ed8f38ec
ThomasLynn <thomaslynn01@gmail.com>
2021-05-01 23:04:41 +0100

    cleaned up server file

--- server.py
+++ server.py
@@ -9,6 +9,21 @@ import pigpio
 
 pi = pigpio.pi()
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-ip", default="",
+    help="Specify an ip the server should be bound to, default is all.")
+parser.add_argument("-port", default="3647", help="The port the server should be bound to.")
+parser.add_argument("-x_servo", default="12", help="GPIO pin of servo controlled by x axis movement")
+parser.add_argument("-y_servo", default="13", help="GPIO pin of servo controlled by y axis movement")
+args = parser.parse_args()
+
+HOST, PORT = args.ip, int(args.port)
+
+servos = myservo.Servo(int(args.x_servo),pi),myservo.Servo(int(args.y_servo),pi)
+
+server = None
+
+# read the data and sets servos to their new positions
 def set_position(data):
     print("got data",type(data),data)
     for i in range(len(data)):
@@ -22,47 +37,29 @@ def set_position(data):
         servos[i].set_angle(data[i], False)
         servos[i].chill_bro()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-ip", default="",
-    help="Specify an ip the server should be bound to, type ipconfig (or ifconfig) in the command line to get your local ip.")
-parser.add_argument("-port", default="3647", help="The port the server should be bound to.")
-parser.add_argument("-x_servo", default="12", help="GPIO pin of servo controlled by x axis movement")
-parser.add_argument("-y_servo", default="13", help="GPIO pin of servo controlled by y axis movement")
-args = parser.parse_args()
-
-HOST, PORT = args.ip, int(args.port)
-
-servos = myservo.Servo(int(args.x_servo),pi),myservo.Servo(int(args.y_servo),pi)
-
 # Create the server, binding to localhost on port 3647
-server = None
 try:
     sock = socket.socket(socket.AF_INET, # Internet
                           socket.SOCK_DGRAM) # UDP
     sock.bind((HOST, PORT))
     
-    sock.settimeout(.02)
+    sock.settimeout(.1)
     
-    #server.timeout = 0.1
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     while True:
         try:
-            data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+            data, addr = sock.recvfrom(1024)
             print("data",data,"addr",addr)
             set_position(data)
         except socket.timeout:
             pass
         for w in servos:
             w.chill_bro()
-    #server.serve_forever()
                 
 except KeyboardInterrupt:
     pass
 finally:
-    #for w in servos:
-    #    w.stop()
-    #GPIO.cleanup()
     pi.stop()
     if server!=None:
         server.server_close()
